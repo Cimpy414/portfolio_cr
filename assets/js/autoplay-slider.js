8a33961994aa5deabf45b18ab8bfb31a9fb50229
cindy <cimpylula14@hotmail.de>
2023-08-09 10:05:14 +0200

    init commit

---
+++ assets/js/autoplay-slider.js
@@ -0,0 +1,10 @@
+//Autoplay Slider
+$('.autoplay-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0, 
+  speed: 4000, 
+  infinite: true,
+  cssEase:'linear',
+});
